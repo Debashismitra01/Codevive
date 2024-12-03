7b119a70a6c0f0aaf171ee89b6f455a6ae65e88b
Debashis Mitra <mdebashis268@gmail.com>
2024-12-03 11:30:46 +0530

    Update navbar.tsx

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar(){
     return(
         <div className="navbar">
             <h1>Codevive</h1>
-            <a>Download</a>
+            <a href="https://drive.google.com/uc?export=download&id=1xK4qbgudcYv891b3XePqPVpP24AMRio4" download>Download</a>
         </div>
     );
 }
